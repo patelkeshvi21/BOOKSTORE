f8d692b114c16c4e2a1d4e75e0bea97c00adabcf
keshvi <22it096@charusat.edu.in>
2024-03-30 16:41:37 +0530

    my first commit

---
+++ Backend/config.js
@@ -0,0 +1,6 @@
+ export const PORT = process.env.PORT || 5555;
+ export const mongoDBURL =
+ "mongodb+srv://ROOT:ROOT@bookstore.vrmrsuv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BOOKSTORE"
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
